b7d3993fadf491b77255a158df1fda33394115b0
Sergey Evdokimov <Sergey.Evdokimov@cern.ch>
2024-09-23 13:54:48 +0200

    Remove leftovers

--- source/Shielding_10K_8/g4tgeoConfig.C
+++
@@ -1,1 +0,0 @@
-../g4tgeoConfig.C
--- source/env.sh
+++ source/env.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/evdokimov/hyperon/neutronMCStudy/install/lib
-export G4LENDDATA=/home/evdokimov/mysw/INSTALL/geant4/share/Geant4/data/LEND_GND1.3_ENDF.BVII.1
+export G4LENDDATA=$MYSW/INSTALL/geant4/share/Geant4/data/LEND_GND1.3_ENDF.BVII.1
